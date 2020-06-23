1dde7abf40fcd3d6b2fe47ca7e626e7acefaa086
Kevin Hillinger <kevin.hillinger@microsoft.com>
2020-06-23 13:14:40 -0400

    moving product subgroup to operations folder

--- src/azure-cli/azure/cli/command_modules/apim/commands.py
+++ src/azure-cli/azure/cli/command_modules/apim/commands.py
@@ -33,6 +33,12 @@ def load_command_table(self, _):
         client_factory=cf_product
     )
 
+    product_custom_type = CliCommandType(
+        operations_tmpl='azure.cli.command_modules.apim.operations.product#{}',
+        client_factory=cf_product
+    )
+
+    # pylint: disable=line-too-long
     with self.command_group('apim', service_sdk, is_preview=True) as g:
         g.custom_command('create', 'create_apim', supports_no_wait=True, table_transformer=service_output_format)
         g.custom_show_command('show', 'get_apim', table_transformer=service_output_format)
@@ -51,10 +57,10 @@ def load_command_table(self, _):
         g.custom_command('delete', 'delete_policy', confirmation=True, supports_no_wait=True)
         g.custom_command('update', 'update_policy', table_transformer=None, validator=validate_policy_xml_content)
 
-    # product apis
-    with self.command_group('apim product', product_sdk, is_preview=True, client_factory=cf_product) as g:
-        g.custom_command('create', 'product_create', table_transformer=product_output_format)
-        g.custom_command('list', 'product_list_by_service', table_transformer=product_output_format)
-        g.custom_command('show', 'product_show', table_transformer=product_output_format)
-        g.custom_command('delete', 'product_delete', table_transformer=product_output_format)
-        g.generic_update_command('update', custom_func_name='product_update', getter_name='get', setter_name='create_or_update', supports_no_wait=True)
+    # product
+    with self.command_group('apim product', product_sdk, custom_command_type=product_custom_type, is_preview=True, client_factory=cf_product) as g:
+        g.custom_command('create', 'create_product', table_transformer=product_output_format)
+        g.custom_command('list', 'list_product', table_transformer=product_output_format)
+        g.custom_command('show', 'get_product', table_transformer=product_output_format)
+        g.custom_command('delete', 'delete_product', table_transformer=product_output_format)
+        g.generic_update_command('update', custom_func_name='update_product', getter_name='get', setter_name='create_or_update', supports_no_wait=True)
--- src/azure-cli/azure/cli/command_modules/apim/custom.py
+++ src/azure-cli/azure/cli/command_modules/apim/custom.py
@@ -7,7 +7,7 @@
 from azure.cli.core.util import sdk_no_wait
 from azure.mgmt.apimanagement.models import (ApiManagementServiceResource, ApiManagementServiceIdentity,
                                              ApiManagementServiceSkuProperties, ApiManagementServiceBackupRestoreParameters,
-                                             VirtualNetworkType, SkuType, ProductContract)
+                                             VirtualNetworkType, SkuType)
 
 
 def create_apim(client, resource_group_name, name, publisher_email, sku_name=SkuType.developer.value,
@@ -111,81 +111,3 @@ def apim_apply_network_configuration_updates(client, resource_group_name, name, 
         properties['location'] = location
 
     return client.api_management_service.apply_network_configuration_updates(resource_group_name, name, properties)
-
-
-# Product Commands
-def product_list_by_service(client, resource_group_name, service_name):
-    """Lists a collection of products in the specified service instance. """
-
-    return client.list_by_service(resource_group_name, service_name)
-
-
-def product_show(client, resource_group_name, service_name, product_id):
-    """Gets the details of the product specified by its identifier. """
-
-    return client.get(resource_group_name, service_name, product_id)
-
-
-def product_delete(client, resource_group_name, service_name, product_id):
-    """Delete product. """
-    delete_subscriptions = 'True'
-    # Request Header If-Match
-    if_match = '*'
-    return client.delete(resource_group_name, service_name, product_id, delete_subscriptions, if_match)
-
-
-def product_create(client, resource_group_name, service_name, product_id, description=None, terms=None,
-                   subscription_required=None, approval_required=None, subscriptions_limit=None, state=None):
-    """Creates a product. """
-    # Request Header If-Match
-    if_match = '*'
-
-    resource = ProductContract(
-        display_name=product_id
-    )
-    if description is not None:
-        resource.description = description
-
-    if terms is not None:
-        resource.terms = terms
-
-    if subscription_required is not None:
-        resource.subscription_required = subscription_required
-
-    if approval_required is not None:
-        resource.approval_required = approval_required
-
-    if subscriptions_limit is not None:
-        resource.subscriptions_limit = subscriptions_limit
-
-    if state is not None:
-        resource.state = state
-
-    return client.create_or_update(resource_group_name, service_name, product_id, resource, if_match)
-
-
-def product_update(instance, description=None, terms=None, subscription_required=None, approval_required=None,
-                   subscriptions_limit=None, state=None):
-    """Update existing product details. """
-    # Request Header If-Match
-#    if_match = '*'
-
-    if description is not None:
-        instance.description = description
-
-    if terms is not None:
-        instance.terms = terms
-
-    if subscription_required is not None:
-        instance.subscription_required = subscription_required
-
-    if approval_required is not None:
-        instance.approval_required = approval_required
-
-    if subscriptions_limit is not None:
-        instance.subscriptions_limit = subscriptions_limit
-
-    if state is not None:
-        instance.state = state
-
-    return instance
