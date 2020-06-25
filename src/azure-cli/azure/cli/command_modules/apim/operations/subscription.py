21825d1bb1c08e6cf75d967ce324f52d8b1e62f0
Pete Rodriguez <perktime@users.noreply.github.com>
2020-06-25 15:16:57 -0500

    Made enhancements to apim subscription

--- src/azure-cli/azure/cli/command_modules/apim/operations/subscription.py
+++ src/azure-cli/azure/cli/command_modules/apim/operations/subscription.py
@@ -14,8 +14,35 @@ def get_subscription(client, resource_group_name, service_name, sid):
     return client.get(resource_group_name, service_name, sid)
 
 
-def create_subscription(client, resource_group_name, service_name, sid):
-    return client.create(resource_group_name, service_name, sid)
+def create_subscription(cmd, resource_group_name, service_name, sid, display_name, scope, owner_id=None, primary_key=None, secondary_key=None, state=None, allow_tracing=None):
+    from azure.cli.command_modules.apim._client_factory import (cf_service, cf_subscription)
+        
+    client = cf_subscription(cmd.cli_ctx)
+    service_client = cf_service(cmd.cli_ctx)
+    apim_instance = service_client.get(resource_group_name, service_name)
+    scope = apim_instance.id + "/" + scope
+
+    parameters = SubscriptionCreateParameters(
+        display_name = display_name,
+        scope = scope        
+    )
+    if owner_id is not None:
+        owner_id = apim_instance.id + "/users/" + owner_id
+        parameters.owner_id = owner_id
+
+    if primary_key is not None:
+        parameters.primary_key = primary_key
+
+    if secondary_key is not None:
+        parameters.secondary_key = secondary_key
+
+    if state is not None:
+        parameters.state = state
+
+    if allow_tracing is not None:
+        parameters.allow_tracing = allow_tracing
+
+    return client.create_or_update(resource_group_name, service_name, sid, parameters)
 
 
 def update_subscription(client, resource_group_name, service_name):
